3320a15ae9b28655ea042cad38f42fc96d9f9959
kecoma1 <kecoma2@gmail.com>
2021-04-08 16:21:16 +0200

    Lab 4

---
+++ LAB_4/pr4/vehicles/IVehicle.java
@@ -0,0 +1,41 @@
+/**
+ * Interface to define a vehicle.
+ * @author Kevin de la Coba and Marcos Bernuy.
+ */
+public interface IVehicle {
+    /**
+     * Method to get the position of the vehicle.
+     * @return Position of the vehicle.
+     */
+    public double getActualPosition();
+
+    /**
+     * Method to get the actual position of the vehicle.
+     * @param newPosition Position to be setted.
+     */
+    public void setActualPosition(double newPosition);
+
+    /**
+     * Method to check if the vehicle can move.
+     * @return Boolean value to see if the vehicle can move.
+     */
+    public boolean canMove();
+
+    /**
+     * Method to allow or not the vehicle to move. 
+     * @param newMovement Value to be set.
+     */
+    public void setCanMove(boolean newMovement);
+
+    /**
+     * Method to get the maximum speed of a vehicle.
+     * @return Maximum speed of the vehicle.
+     */
+    public double getMaxSpeed();
+
+    /**
+     * Method to get the name of a vehicle.
+     * @return Name of the vehicle.
+     */
+    public String getName();
+}
