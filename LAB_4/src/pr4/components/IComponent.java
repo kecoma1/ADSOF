9df8439d32567bfd95071fa2df81d19a50c63add
kecoma1 <kecoma2@gmail.com>
2021-04-22 16:07:34 +0200

    Component interface added

---
+++ LAB_4/src/pr4/components/IComponent.java
@@ -0,0 +1,46 @@
+/**
+ * Interface to define components and their behaviours.
+ * @author Kevin de la Coba and Marcos Aarón Bernuy.
+ */
+public interface IComponent {
+    /**
+     * Method to check if a component is damaged.
+     * @return True if it is, False if not.
+     */
+    public boolean isDamaged();
+
+    /**
+     * Method to set if a component is damaged or not.
+     * @param damage Value of the damage, true or false.
+     */
+    public void setDamaged(boolean damage);
+
+    /**
+     * Method to get the name of the component.
+     * @return String with the name.
+     */
+    public String getName();
+
+    /**
+     * Method to get the cost of repairing.
+     * @return Cost of the repairing, turns.
+     */
+    public int costRepair();
+
+    /**
+     * Vehicle of the component.
+     * @return Vehicle of the component.
+     */
+    public IVehicle getVehicle();
+
+    /**
+     * If the damaged in a specific component is critical.
+     * @return Boolean value.
+     */
+    public boolean isCritical();
+
+    /**
+     * Method to repair the component.
+     */
+    public void repair();
+}
